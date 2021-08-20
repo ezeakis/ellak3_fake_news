c8db98be1161159686881faf9c11413c5f3f1499
Manos Zeakis <ezeakis@gmail.com>
2021-08-20 05:40:15 +0300

    Add files via upload

--- Fake-News-Fighter-team-code.py
+++ Fake-News-Fighter-team-code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[4]:
 
 
 #Φέρνουμε τη βιβλιοθήκη numpy και την ονομάζουμε np
@@ -11,12 +11,13 @@ import numpy as np
 import pandas as pd
 
 
-# In[22]:
+# In[5]:
 
 
-#Φορτώνουμε τα δεδομένα του αρχείου True.csv στη μεταβλητή "true"
+#Φορτώνουμε τα δεδομένα του αρχείου Fake.csv στη μεταβλητή "fake"
 fake = pd.read_csv("Fake.csv")
 
+#Μετράμε πόσες φορές εμφανίζονται οι "κρίσιμες λέξεις" και προσθέτουμε τις αντίστοιχες στήλες
 fake["to"] = fake['title'].str.count("to ")
 fake["in"] = fake['title'].str.count("in ")
 fake["on"] = fake['title'].str.count("on ")
@@ -24,20 +25,21 @@ fake["On"] = fake['title'].str.count("On ")
 fake["with"] = fake['title'].str.count("with ")
 fake["THE"] = fake['title'].str.count("THE ")
 
+#Προσθέτουμε τις στήλες για τις κλάσεις
 fake['target'] = 0
-
 fake['Validity'] = "fake"
 
 #Εμφανίζουμε τις πέντε πρώτες γραμμές των δεδομένων μας
 fake.head()
 
 
-# In[23]:
+# In[6]:
 
 
 #Φορτώνουμε τα δεδομένα του αρχείου True.csv στη μεταβλητή "true"
 true = pd.read_csv("True.csv")
 
+#Μετράμε πόσες φορές εμφανίζονται οι "κρίσιμες λέξεις" και προσθέτουμε τις αντίστοιχες στήλες
 true["to"] = true['title'].str.count("to ")
 true["in"] = true['title'].str.count("in ")
 true["on"] = true['title'].str.count("on ")
@@ -45,22 +47,22 @@ true["On"] = true['title'].str.count("On ")
 true["with"] = true['title'].str.count("with ")
 true["THE"] = true['title'].str.count("THE ")
 
-
+#Προσθέτουμε τις στήλες για τις κλάσεις
 true['target'] = 1
-
 true['Validity'] = "true"
 
 #Εμφανίζουμε τις πέντε πρώτες γραμμές των δεδομένων μας
 true.head()
 
 
-# In[24]:
+# In[7]:
 
 
+#Ενώνουμε τους πίνακες fake και true
 frames = [true, fake]
-
 result = pd.concat(frames)
 
+#Κρατάμε μόνο τα δεδομένα που θα χρειαστούμε
 result.drop('text', axis='columns', inplace=True)
 result.drop('subject', axis='columns', inplace=True)
 result.drop('date', axis='columns', inplace=True)
@@ -69,90 +71,77 @@ result.drop('title', axis='columns', inplace=True)
 result.tail(20)
 
 
-# In[25]:
+# In[8]:
 
 
+#Απομονώνουμε τα features
 result_feat = pd.DataFrame(result.drop(result.columns[[6,7]], axis=1))
-
-
-# In[26]:
-
-
 result_feat.head()
 
 
-# In[27]:
+# In[9]:
 
 
+#Ελέγχουμε το target
 result['target'].head()
 
 
-# In[28]:
+# In[10]:
 
 
+#Χωρίζουμε τα δεδομένα μας σε δεδομένα εκπαίδευσης/δοκιμής
 from sklearn.model_selection import train_test_split
-
-
-# In[29]:
-
-
 X_train, X_test, y_train, y_test = train_test_split(result_feat,result['target'],
                                                     test_size=0.30)
 
 
-# In[30]:
+# In[11]:
 
 
+#Αν δεν υπάρχει ο xgboost, τον εγκαθιστούμε με τις παρακάτω εντολές
 #pip install xgboost
 #or
 #conda install -c conda-forge xgboost
 
 from xgboost import XGBClassifier
 
-
-# In[31]:
-
-
+#Ο AdaBoostClassifier είναι εναλλακτικός
 #adb = AdaBoostClassifier(n_estimators=100, random_state=0)
 
+#Ορίζουμε τον Classifier
 xgb = XGBClassifier(n_estimators=100, random_state=0)
 
 
-# In[32]:
+# In[12]:
 
 
+#Εκπαιδεύουμε τον classifier από τα δεδομένα μας
 xgb.fit(X_train,y_train)
 
 
-# In[33]:
+# In[13]:
 
 
+#Ζητάμε από τον Classifier να ελέγξει τα δεδομένα δοκιμής αν είναι αληθινά ή ψευδή
 pred = xgb.predict(X_test) 
  
 
 
-# In[34]:
+# In[14]:
 
 
-"""evaluate predictions"""
-
+#Ελέγχουμε αν τα πήγε καλά
 predictions = [round(value) for value in pred]
-
 from sklearn.metrics import accuracy_score
-
 accuracy = accuracy_score(y_test, predictions)
-
-#print(accuracy)
-
-
 print(f"Accuracy: {accuracy * 100.0}%")
 
 
-# In[42]:
+# In[15]:
 
 
+#Βάζουμε νέους τίτλους για να ελέγξει ο Classifier αν είναι αληθινοί οι ψεύτικοι
 #data = [{'title':'Three things to watch as Joe Biden meets Vladimir Putin'}]
-
 data = [{'title':'Israel strikes in Gaza after fire balloons launched'}]
 
 df = pd.DataFrame(data)
@@ -168,16 +157,10 @@ df.drop('title', axis='columns', inplace=True)
 print(df)
 
 
-# In[43]:
+# In[16]:
 
 
+#Ζητάμε την πρόβλεψη του Classifier
 pred = xgb.predict(df)
-
 print(pred)
-
-
-# In[ ]:
-
-
-
 
